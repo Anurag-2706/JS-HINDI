10530c1278c78d7735f9826f819f97525ca1dc94
Anurag-2706 <anurag.workplace@gmail.com>
2024-08-23 10:04:31 +0000

    data types

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all js code as newer version
+//alert(3+3) we are using nodejs,not browser
+//data types
+// number = 2 to the powwr 53
+//bigint
+//string = ""
+//boolean =true/false
+// null= standalonevalue
+ //undefined
+ //object
+ let name="anurag"
+ console.log(typeof "hitesh");
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,9 @@
-console.log("xyz")
+const accountId = 14435
+let accountemail = "abc@gmail.com"
+var accountpassword = "12345"
+accountcity= "jaipur"
+console.table([accountemail,accountId,accountpassword,accountcity])
+accountemail = "xyz@gmail.com"
+accountpassword = "7890"
+accountcity= "delhi"
+console.table([accountemail,accountId,accountpassword,accountcity])
